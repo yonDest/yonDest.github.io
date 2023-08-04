d1e885749379ddb883006aadfa78a72e6d6254dc
Yoni Desta <yonidesta9@gmail.com>
2023-08-03 18:52:02 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var text = 'Widget World';
-
+const tl = new TimelineMax()
 var textElements = text.split("").map(function(c) {
   return $('<span id="' + c + '">' + c + '</span>');
 });
-
+tl.fromTo(".type-me", 5, {
+  width: "0",
+}, {
+  width: "870px", /* same as text-description width*/
+}, 0);
 var el = $('#letters');
 var delay = 50; // Tune this for different letter delays.
 textElements.forEach(function(e, i) {
