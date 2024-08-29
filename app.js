d70c0c89270cf27d136fb93838ba93da9a0ec4b0
Yoni Desta <yonidesta9@gmail.com>
2024-08-28 22:41:22 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ var text = 'Widget World';
 var textElements = text.split("").map(function(c) {
   return $('<span id="' + c + '">' + c + '</span>');
 });
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
 
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
 var el = $('#letters');
 var delay = 50; // Tune this for different letter delays.
 textElements.forEach(function(e, i) {
