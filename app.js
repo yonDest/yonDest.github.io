28edb8491f73c4a77bba74f235c8dc1e1c14694f
Yoni Desta <yonidesta9@gmail.com>
2024-08-29 20:14:55 -0600

    Update app.js

--- app.js
+++ app.js
@@ -19,10 +19,10 @@ const header = document.querySelector('.navbar');
 
 window.onscroll = function() {
     var top = window.scrollY;
-    if(top >=100) {
+    if(top >= 100) {
         header.classList.add('navbarDark');
     }
     else {
         header.classList.remove('navbarDark');
     }
-});
+}
